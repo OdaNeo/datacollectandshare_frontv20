17e6034e31bf66ea472ed93b791052f828619fd6
wangzhiwei <wangzhiwei3306@163.com>
2020-09-11 11:07:21 +0800

    修改重置的bug

--- src/components/h-form.vue
+++ src/components/h-form.vue
@@ -32,7 +32,7 @@
                         height="32"
                         solo
                         v-if="formProvide.btnName.length>0&&!formProvide.btnName[1]"
-                        @click="resetData"
+                        @click="reset(true)"
                     >
                         重置
                     </v-btn>
@@ -69,17 +69,24 @@ export default class DialogForm extends Vue{
         const parent = this.$parent.$parent.$parent.$parent as any
         parent.closeMethod(e)
     }
-    private resetData(){
+    private reset(clearAll:boolean){
         const formObj:any = this.formProvide.formObj
-        if(formObj.title = "添加字段"){
+        if(clearAll){
             // 增加字段的重置 至重置数据结构部分
-            for(let i=0;i<formObj.topicList.length;i++){
-                if(!formObj.topicList[i].disabled){
-                    formObj.topicList.splice(i, 1)
+            if(formObj.title = "添加字段"){
+                // 增加字段的重置 至重置数据结构部分
+                for(let i=0;i<formObj.topicList.length;i++){
+                    console.log(formObj.topicList[i])
+                    if(!formObj.topicList[i].disabled&&formObj.topicList.length!=1){
+                        console.log('从第几个开始删除',i)
+                        console.log('删除几个',formObj.topicList.length-i)
+                        formObj.topicList.splice(i, formObj.topicList.length-i)
+                    }
                 }
+                return
             }
-            return
-        }
+        }else{
+        // this.udf.reset()
         for(let key in formObj){
             if(Object.prototype.toString.call(formObj[key]) != '[object Object]'){
                 if(Object.prototype.toString.call(formObj[key])=='[object Array]'){
@@ -122,51 +129,6 @@ export default class DialogForm extends Vue{
         }
         this.udf.resetValidation()
     }
-    private reset(){
-        // 增加字段的重置 至重置数据结构部分
-        // this.udf.reset()
-        const formObj = this.formProvide.formObj
-        for(let key in formObj){
-            if(Object.prototype.toString.call(formObj[key]) != '[object Object]'){
-                if(Object.prototype.toString.call(formObj[key])=='[object Array]'){
-                    formObj[key] = []
-                }else{
-                    formObj[key] = ""
-                }
-            }else{
-                if((formObj[key] as FormStructure).reset){
-                    (formObj[key] as FormStructure).text = this.formProvideBF[key].text
-                }else{
-                    (formObj[key] as FormStructure).text = ""
-                }
-            }
-        }
-        if((this.$children[0].$children[0] as any).clearMethod){
-            (this.$children[0].$children[0] as any).clearMethod()
-        }
-
-        this.formProvide.formObj = {
-            id:'', // 主题ID
-            canNotEdit: false, // 添加数据
-            interfaceType:1,
-            topicName:'', // 主题名称
-            messageType:'', // 消息类型
-            dataBaseIp:'', // 数据库地址
-            databaseType:'', // 数据库类型
-            redisTimer: '',
-            header: [{key:'',value:''},],
-            url: '',
-            topicList:[
-                {
-                    number: '',
-                    key:'',
-                    type:'',
-                    description:'', // 描述
-                    disabled:false
-                }
-            ]
-        }
-        this.udf.resetValidation()
     }
 }
 </script>
