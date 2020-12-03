f1b9fef9dca3c3b2378f651739601ba8043aa0e0
OdaNeo <odaneo@outlook.com>
2020-12-03 09:58:07 +0800

    主题名称校验规则添加@

--- src/validator/topic-validator.ts
+++ src/validator/topic-validator.ts
@@ -61,7 +61,7 @@ class TopicValidator{
     return [
       (v:string) =>!!v||"主题名称不能为空",
       (v:string) =>(v&&v.length<=20) || "主题名称最长可设置20个字符",
-      (v:string) => /^\w*$/.test(v) || "内容只能为数字、字母、下划线的组合"
+      (v:string) => /^[\w@]*$/.test(v) || "内容只能为数字、字母、下划线、@的组合"
     ]
   }
 
