3d25467ec9bc3a9115e111d76d346837939121f2
OdaNeo <odaneo@outlook.com>
2021-02-22 18:43:04 +0800

    add: 接口问题排查 add: dataX 回显

--- src/views/core/transactionalData/childComponent/jsonContentDetails.vue
+++ src/views/core/transactionalData/childComponent/jsonContentDetails.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="JsonContentDetails">
-    <v-container>
-      {{ rowJson }}
-    </v-container>
+    <v-container v-html="obj"></v-container>
   </div>
 </template>
 <script lang="ts">
@@ -10,6 +8,95 @@ import { Component, Vue, Prop } from 'vue-property-decorator'
 
 @Component({})
 export default class JsonContentDetails extends Vue {
-  @Prop() private rowJson!: unknown
+  @Prop() private rowJson!: string
+
+  private arrayToHTML(arr: Array<unknown>) {
+    let html = ''
+    arr.forEach((item: unknown) => {
+      if (Array.isArray(item)) {
+        // 数组
+        html += `
+        <div style="font-size: 16px;line-height: 24px;">
+          <span>[</span>
+          <div style="margin-left:24px">${this.arrayToHTML(item)}</div>
+          <span>],</span>
+        </div>
+        `
+      } else if (Object.prototype.toString.call(item) === '[object Object]') {
+        // 对象
+        html += `
+        <div style="font-size: 16px;line-height: 24px;">
+          <span>{</span>
+          <div style="margin-left:24px">${this.objToHTML(item)}</div>
+          <span>},</span>
+        </div>
+        `
+      } else if (typeof item === 'string') {
+        // string
+        html += `
+        <div style="font-size: 16px;line-height: 24px;">
+          <span>"${item}"</span><span>,</span>
+        </span>
+        `
+      } else {
+        // 其他
+        html += `
+        <div style="font-size: 16px;line-height: 24px;">
+          <span>${item}</span><span>,</span>
+        </span>
+        `
+      }
+    })
+    return html
+  }
+
+  private objToHTML(obj: any) {
+    let html = ''
+    for (const p in obj) {
+      if (Array.isArray(obj[p])) {
+        // 数组
+        html += `
+        <div style="font-size: 16px;line-height: 24px;">
+          <span style="font-size: 16px;over-flow: auto;">"${p}":</span>
+          <span>[</span>
+          <div style="margin-left:24px">${this.arrayToHTML(obj[p])}</div>
+          <span>],</span>
+        </div>
+        `
+      } else if (Object.prototype.toString.call(obj[p]) === '[object Object]') {
+        // 对象
+        html += `
+        <div style="font-size: 16px;line-height: 24px;">
+          <span style="font-size: 16px;over-flow: auto;">"${p}":</span>
+          <span>{</span>
+          <div style="margin-left:24px">${this.objToHTML(obj[p])}</div>
+          <span>},</span>
+        </div>
+        `
+      } else if (typeof obj[p] === 'string') {
+        // string
+        html += `
+        <div style="font-size: 16px;line-height: 24px;">
+          <span style="font-size: 16px;over-flow: auto;">"${p}":</span>
+          <span>"${obj[p]}"</span><span>,</span>
+        </div>
+        `
+      } else {
+        // string
+        html += `
+        <div style="font-size: 16px;line-height: 24px;">
+          <span style="font-size: 16px;over-flow: auto;">"${p}":</span>
+          <span>${obj[p]}</span><span>,</span>
+        </div>
+        `
+      }
+    }
+    return html
+  }
+
+  private get obj() {
+    const _obj = JSON.parse(this.rowJson)
+    return `<div style="font-size: 16px;">{</div>${this.objToHTML(_obj)}<div>}</div>`
+  }
 }
 </script>
